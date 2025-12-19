be667553b05bf05077a4df3cec81e54373cc59e2
Brenda0714 <brendacavazos14@outlook.es>
2025-12-19 01:46:02 -0600

    seccion 5 inicio

--- 02-angular-start/src/app/app.routes.ts
+++ 02-angular-start/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CounterPageComponent } from './components/counter/counter-page.component';
 import { tarea1CounterComponent } from './components/counter/tarea1Counter.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
 
@@ -14,6 +15,11 @@ export const routes: Routes = [
     path: 'hero',
 
     component: HeroPageComponent,
+  },
+  {
+    path: 'dragonball',
+
+    component: DragonballPageComponent,
   },
   {
     path: '**',

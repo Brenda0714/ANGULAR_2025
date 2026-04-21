1a32e47da8aa7e740644810ca5f830ff535eaada
Brenda Cavazos <62002152+Brenda0714@users.noreply.github.com>
2026-04-20 22:49:33 -0600

    Move Character interface and add service
    Move the Character interface out of the components folder into src/app/interfaces and update imports accordingly. Add a new DragonballService that holds the characters signal and provides addCharacter(), centralizing state. Refactor DragonballSuperPage to inject and use DragonballService (remove local signal and addCharacter), and update the template to call DragonballService methods. Updated character-add and character-list imports to the new interface path.

--- 02-angular-start/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-angular-start/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,13 +1,7 @@
-
-import { Component, signal } from '@angular/core';
+import { DragonballService } from './../../services/dragonball.service';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
-
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
 
 @Component({
   templateUrl: './dragonball-super-page.component.html',
@@ -16,14 +10,11 @@ interface Character {
 })
 export class DragonballSuperPageComponent {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
+  // constructor(
+  //   public DragonballService: DragonballService
+  // ){}
 
-  addCharacter(newCharacter : Character){
+  public DragonballService = inject(DragonballService);
 
-    this.characters.update((list) => [...list,newCharacter]);
-  }
   
 }

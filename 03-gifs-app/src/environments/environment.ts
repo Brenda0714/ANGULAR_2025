86afb8e980e348b95313c34e686b81b03b50c690
Brenda Cavazos <62002152+Brenda0714@users.noreply.github.com>
2026-04-26 20:56:24 -0600

    Giphy API y GifService Interfaces

--- 03-gifs-app/src/environments/environment.ts
+++ 03-gifs-app/src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
 
-    production: true,
-    companyName: 'Gifs',
-    companyName2: 'App',
-    companySlogan: 'Maneja tus Gifs',
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus Gifs',
 
-    // Apikeys
-
-    // URLS
+  // Apikeys
+  giphyApiKey: 'yBLVR1lmZWLUXrzlYew1ekEsQAwseNCz',
+  // URLS
+  giphyUrl: 'https://api.giphy.com/v1',
 
 };

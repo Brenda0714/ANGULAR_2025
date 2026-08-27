95e8ff81291357ab05a552772058a629487d7c7c
Brenda Cavazos <62002152+Brenda0714@users.noreply.github.com>
2026-08-26 23:16:04 -0600

    argumentos dinamicos por URL

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -2,24 +2,28 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page'),
-        
-        children:[    
-            {
-                path: 'trending',
-                loadComponent: () => import('./gifs/pages/trending-page/trending-page')
-            },
-            {
-                path: 'search',
-                loadComponent: () => import('./gifs/pages/search-page/search-page')
-            },
-        ]
-    },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page'),
 
-    {
-        path: '**',
-        redirectTo: 'dashboard',
-    }
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page')
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page')
+      },
+      {
+        path: 'history/:key',
+        loadComponent: () => import('./gifs/pages/gif-history/gif-history')
+      },
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
 ];
